14eb12cd127bb4ab65a66c8138a60cdf0c200bd6
Paul_Salony <pjsal452@gmail.com>
2020-12-09 17:20:12 -0500

    Created user.py and updated app.py

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask, jsonify, g
 from flask_cors import CORS
+from flask_login import LoginManager ############# added this line
 
 from resources.songs import song # adding this line
+from resources.user import user ############ added this line
 
 import models
 
 DEBUG = True
 PORT = 8000
 
+login_manager = LoginManager() # sets up the ability to set up the session
+
 # Initialize an instance of the Flask class.
 # This starts the website!
 app = Flask(__name__)
+
+###################### added these lines
+
+app.secret_key = "LJAKLJLKJJLJKLSDJLKJASD" ## Need this to encode the session
+login_manager.init_app(app) # set up the sessions on the app
+
+@login_manager.user_loader # decorator function, that will load the user object whenever we access the session, we can get the user
+# by importing current_user from the flask_login
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+###################### added these lines
 
 @app.before_request
 def before_request():
@@ -35,6 +53,13 @@ app.register_blueprint(song, url_prefix='/api/songs')
 @app.route('/')
 def index():
     return 'hi'
+
+################## added these lines
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
+app.register_blueprint(user, url_prefix='/user')
+################## added these lines
+
+
 
 # Run the app when the program starts!
 if __name__ == '__main__':
